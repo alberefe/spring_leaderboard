b103dafaa5ccd81c3ed7918fc5744abad9238522
Alberto Ferrer <alberefe@gmail.com>
2025-08-13 13:28:29 +0200

    Initial commit

---
+++ src/main/java/com/board/spring_leaderboard/SpringLeaderboardApplication.java
@@ -0,0 +1,13 @@
+package com.board.spring_leaderboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLeaderboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringLeaderboardApplication.class, args);
+    }
+
+}
